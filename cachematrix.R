0356d16ff9a474e8aa81ebaaa24b5d1edcc9638f
Ahmed Zakaria Ali <ahmedelhatab@gmail.com>
2020-06-06 23:40:40 +0200

    Adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##this function used to create the matrix object and also the required function 
+## set the values / get the values of the matrix and its inverse.
 makeCacheMatrix <- function(x = matrix()) 
 {
   inverse_matrix <- NULL
@@ -20,10 +21,13 @@ makeCacheMatrix <- function(x = matrix())
 
 
 ## Write a short comment describing this function
+## this function is used to get the inverse matrix if the data is changed
+## return the cached value if the data isn't changed
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inverse_value<-x$getinverse();
+  #check for the inverse matrix 
   if(!is.null(inverse_value))
   {
     print("cached data");
